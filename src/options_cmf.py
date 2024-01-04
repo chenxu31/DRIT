88fc3b9f79ca071d97ebbef79e9a88436481af90
chenxu31 <chenxu31@gmail.com>
2024-01-04 11:01:35 +0800

    sync

---
+++ src/options_cmf.py
@@ -0,0 +1,103 @@
+import argparse
+
+class TrainOptions():
+  def __init__(self):
+    self.parser = argparse.ArgumentParser()
+
+    # data loader related
+    self.parser.add_argument('--dataroot', type=str, required=True, help='path of data')
+    self.parser.add_argument('--phase', type=str, default='train', help='phase for dataloading')
+    self.parser.add_argument('--batch_size', type=int, default=2, help='batch size')
+    self.parser.add_argument('--resize_size', type=int, default=256, help='resized image size for training')
+    self.parser.add_argument('--crop_size', type=int, default=216, help='cropped image size for training')
+    self.parser.add_argument('--input_dim_a', type=int, default=3, help='# of input channels for domain A')
+    self.parser.add_argument('--input_dim_b', type=int, default=3, help='# of input channels for domain B')
+    self.parser.add_argument('--nThreads', type=int, default=8, help='# of threads for data loader')
+    self.parser.add_argument('--no_flip', action='store_true', help='specified if no flipping')
+
+    # ouptput related
+    self.parser.add_argument('--name', type=str, default='trial', help='folder name to save outputs')
+    self.parser.add_argument('--display_dir', type=str, default='../logs', help='path for saving display results')
+    self.parser.add_argument('--checkpoint_dir', type=str, default='../results', help='path for checkpoint files')
+    self.parser.add_argument('--result_dir', type=str, default='../results', help='path for saving result images')
+    self.parser.add_argument('--display_freq', type=int, default=1000, help='freq (iteration) of display')
+    self.parser.add_argument('--img_save_freq', type=int, default=100000, help='freq (epoch) of saving images')
+    self.parser.add_argument('--model_save_freq', type=int, default=100000, help='freq (epoch) of saving models')
+    self.parser.add_argument('--no_display_img', action='store_true', help='specified if no dispaly')
+
+    # training related
+    self.parser.add_argument('--max_iter', type=int, default=100000, help='max training iterations')
+    self.parser.add_argument('--no_ms', action='store_true', help='disable mode seeking regularization')
+    self.parser.add_argument('--concat', type=int, default=1, help='concatenate attribute features for translation, set 0 for using feature-wise transform')
+    self.parser.add_argument('--dis_scale', type=int, default=3, help='scale of discriminator')
+    self.parser.add_argument('--dis_norm', type=str, default='None', help='normalization layer in discriminator [None, Instance]')
+    self.parser.add_argument('--dis_spectral_norm', action='store_true', help='use spectral normalization in discriminator')
+    self.parser.add_argument('--lr_policy', type=str, default='lambda', help='type of learn rate decay')
+    self.parser.add_argument('--n_ep', type=int, default=1200, help='number of epochs') # 400 * d_iter
+    self.parser.add_argument('--n_ep_decay', type=int, default=600, help='epoch start decay learning rate, set -1 if no decay') # 200 * d_iter
+    self.parser.add_argument('--resume', type=str, default=None, help='specified the dir of saved models for resume the training')
+    self.parser.add_argument('--d_iter', type=int, default=3, help='# of iterations for updating content discriminator')
+    self.parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    self.parser.add_argument('--aug_sigma', type=float, default=0., help="augmentation parameter:sigma")
+    self.parser.add_argument('--aug_points', type=float, default=20, help="augmentation parameter:points")
+    self.parser.add_argument('--aug_rotate', type=float, default=0, help="augmentation parameter:rotate")
+    self.parser.add_argument('--aug_zoom', type=float, default=1., help="augmentation parameter:zoom")
+    self.parser.add_argument('--psnr_threshold', type=float, default=18.0, help="only save the checkpoint file when PSNR reach this threshold")
+    self.parser.add_argument('--do_validation', type=int, default=1, help="whether do validation during training")
+    self.parser.add_argument('--mini', type=int, default=0, help="whether do mini data to avoid memory insufficient issue")
+  def parse(self):
+    self.opt = self.parser.parse_args()
+
+    if self.opt.mini:
+        self.opt.resize_size = self.opt.resize_size // 2
+        self.opt.crop_size = self.opt.crop_size // 2
+
+    args = vars(self.opt)
+    print('\n--- load options ---')
+    for name, value in sorted(args.items()):
+      print('%s: %s' % (str(name), str(value)))
+    return self.opt
+
+class TestOptions():
+  def __init__(self):
+    self.parser = argparse.ArgumentParser()
+
+    # data loader related
+    self.parser.add_argument('--dataroot', type=str, required=True, help='path of data')
+    self.parser.add_argument('--phase', type=str, default='test', help='phase for dataloading')
+    self.parser.add_argument('--resize_size', type=int, default=256, help='resized image size for training')
+    self.parser.add_argument('--crop_size', type=int, default=216, help='cropped image size for training')
+    self.parser.add_argument('--nThreads', type=int, default=4, help='for data loader')
+    self.parser.add_argument('--input_dim_a', type=int, default=3, help='# of input channels for domain A')
+    self.parser.add_argument('--input_dim_b', type=int, default=3, help='# of input channels for domain B')
+    self.parser.add_argument('--a2b', type=int, default=1, help='translation direction, 1 for a2b, 0 for b2a')
+
+    # ouptput related
+    self.parser.add_argument('--num', type=int, default=5, help='number of outputs per image')
+    self.parser.add_argument('--name', type=str, default='trial', help='folder name to save outputs')
+    self.parser.add_argument('--result_dir', type=str, default='../outputs', help='path for saving result images and models')
+    self.parser.add_argument('--checkpoint_dir', type=str, default='../checkpoints', help='path for saving result images and models')
+
+    # model related
+    self.parser.add_argument('--concat', type=int, default=1, help='concatenate attribute features for translation, set 0 for using feature-wise transform')
+    self.parser.add_argument('--no_ms', action='store_true', help='disable mode seeking regularization')
+    #self.parser.add_argument('--resume', type=str, required=True, help='specified the dir of saved models for resume the training')
+    self.parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    self.parser.add_argument('--mini', type=int, default=0, help='mini')
+
+  def parse(self):
+    self.opt = self.parser.parse_args()
+
+    if self.opt.mini:
+        self.opt.resize_size = self.opt.resize_size // 2
+        self.opt.crop_size = self.opt.crop_size // 2
+
+    args = vars(self.opt)
+    print('\n--- load options ---')
+    for name, value in sorted(args.items()):
+      print('%s: %s' % (str(name), str(value)))
+    # set irrelevant options
+    self.opt.dis_scale = 3
+    self.opt.dis_norm = 'None'
+    self.opt.dis_spectral_norm = False
+    return self.opt
---
+++ src/train_cmf.py
@@ -0,0 +1,135 @@
+import torch
+from options_cmf import TrainOptions
+from dataset import dataset_unpair
+from model import DRIT
+from saver import Saver
+import sys
+import os
+import numpy
+import pdb
+import skimage.io
+
+if platform.system() == 'Windows':
+    UTIL_DIR = r"E:\我的坚果云\sourcecode\python\util"
+else:
+    UTIL_DIR = r"/home/chenxu/我的坚果云/sourcecode/python/util"
+
+sys.path.append(UTIL_DIR)
+import common_metrics
+import common_cmf_pt as common_cmf
+
+def main():
+  # parse options
+  parser = TrainOptions()
+  opts = parser.parse()
+
+  if opts.gpu >= 0:
+      os.environ["CUDA_VISIBLE_DEVICES"] = str(opts.gpu)
+      device = torch.device("cuda")
+  else:
+      os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+      device = torch.device("cpu")
+
+  if not os.path.exists(opts.checkpoint_dir):
+    os.makedirs(opts.checkpoint_dir)
+
+  # daita loader
+  print('\n--- load dataset ---')
+
+  torch.autograd.set_detect_anomaly(True)
+
+  data_iter = common_cmf.DataIterUnpaired(opts.dataroot, device, patch_depth=opts.input_nc,
+                                          batch_size=opts.batch_size)
+  if opts.do_validation:
+    val_data_t, val_data_s, _ = common_cmf.load_test_data(opts.dataroot)
+
+  # model
+  print('\n--- load model ---')
+  model = DRIT(opts)
+  model.setgpu(0)
+  if opts.resume is None:
+    model.initialize()
+    ep0 = -1
+    total_it = 0
+  else:
+    ep0, total_it = model.resume(opts.resume)
+  model.set_scheduler(opts, last_ep=ep0)
+  ep0 += 1
+  print('start the training at epoch %d'%(ep0))
+
+  # saver for display and output
+  saver = Saver(opts)
+
+  # train
+  print('\n--- train ---')
+  best_psnr = 0
+  for it in range(opts.max_iter):
+    images_a, images_b, _ = data_iter.next()
+
+    # update model
+    if (it + 1) % opts.d_iter != 0 and it < opts.max_iter - 2:
+      model.update_D_content(images_a, images_b)
+      continue
+    else:
+      model.update_D(images_a, images_b)
+      model.update_EG()
+
+    if (it + 1) % opts.display_freq == 0 and opts.do_validation:
+      val_st_psnr = numpy.zeros((val_data_s.shape[0], 1), numpy.float32)
+      val_ts_psnr = numpy.zeros((val_data_t.shape[0], 1), numpy.float32)
+      val_st_list = []
+      val_ts_list = []
+      with torch.no_grad():
+        for i in range(val_data_s.shape[0]):
+          val_st = numpy.zeros(val_data_s.shape[1:], numpy.float32)
+          val_ts = numpy.zeros(val_data_t.shape[1:], numpy.float32)
+          used = numpy.zeros(val_data_s.shape[1:], numpy.float32)
+          for j in range(val_data_s.shape[1] - opts.input_dim_a + 1):
+            val_patch_s = torch.tensor(val_data_s[i:i + 1, j:j + opts.input_dim_a, :, :], device=device)
+            val_patch_t = torch.tensor(val_data_t[i:i + 1, j:j + opts.input_dim_a, :, :], device=device)
+
+            ret_st = model.test_forward_transfer(val_patch_s, val_patch_t, a2b=True)
+            ret_ts = model.test_forward_transfer(val_patch_t, val_patch_s, a2b=False)
+
+            val_st[j:j + opts.input_dim_a, :, :] += ret_st.cpu().detach().numpy()[0]
+            val_ts[j:j + opts.input_dim_a, :, :] += ret_ts.cpu().detach().numpy()[0]
+            used[j:j + opts.input_dim_a, :, :] += 1
+
+          assert used.min() > 0
+          val_st /= used
+          val_ts /= used
+
+          st_psnr = common_metrics.psnr(val_st, val_data_t[i])
+          ts_psnr = common_metrics.psnr(val_ts, val_data_s[i])
+
+          val_st_psnr[i] = st_psnr
+          val_ts_psnr[i] = ts_psnr
+          val_st_list.append(val_st)
+          val_ts_list.append(val_ts)
+
+      msg = "Iter:%d  val_st_psnr:%f/%f  val_ts_psnr:%f/%f" % \
+             (it + 1, val_st_psnr.mean(), val_st_psnr.std(), val_ts_psnr.mean(), val_ts_psnr.std())
+      gen_images_test = numpy.concatenate([val_data_s[0], val_st_list[0], val_ts_list[0], val_data_t[0]], 2)
+      gen_images_test = numpy.expand_dims(gen_images_test, 0).astype(numpy.float32)
+      gen_images_test = common_cmf.generate_display_image(gen_images_test, is_seg=False)
+
+      if opts.display_dir:
+        try:
+          skimage.io.imsave(os.path.join(opts.display_dir, "gen_images_test.jpg"), gen_images_test)
+        except:
+          pass
+
+      if val_ts_psnr.mean() > best_psnr:
+        best_psnr = val_ts_psnr.mean()
+
+        if best_psnr > opts.psnr_threshold:
+          model.save(os.path.join(opts.checkpoint_dir, "best.pth"), it, opts.max_iter)
+
+      msg += "  best_ts_psnr:%f" % best_psnr
+      print(msg)
+
+  model.save(os.path.join(opts.checkpoint_dir, "final.pth"), it, opts.max_iter)
+
+
+if __name__ == '__main__':
+  main()
