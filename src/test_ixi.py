a5fa28f1d8ec7d44874b3b6a1a211bd304e0d6be
chenxu31 <chenxu31@gmail.com>
2024-02-08 10:31:58 +0800

    sync

---
+++ src/test_ixi.py
@@ -0,0 +1,115 @@
+import torch
+from options_ixi import TestOptions
+from dataset import dataset_single
+from model import DRIT
+from saver import save_imgs
+import os
+import sys
+import numpy
+import pdb
+import skimage.io
+from skimage.metrics import structural_similarity as ssim
+import platform
+
+if platform.system() == 'Windows':
+  sys.path.append(r"E:\我的坚果云\sourcecode\python\util")
+else:
+  sys.path.append("/home/chenxu/我的坚果云/sourcecode/python/util")
+import common_metrics
+import common_ixi
+
+def main():
+  # parse options
+  parser = TestOptions()
+  opts = parser.parse()
+
+  if opts.gpu >= 0:
+      os.environ["CUDA_VISIBLE_DEVICES"] = str(opts.gpu)
+      device = torch.device("cuda")
+  else:
+      os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+      device = torch.device("cpu")
+
+  if opts.result_dir and not os.path.exists(opts.result_dir):
+      os.makedirs(opts.result_dir)
+
+  test_data_t, test_data_s = common_ixi.load_test_data(opts.dataroot, "test")
+
+  # model
+  print('\n--- load model ---')
+  model = DRIT(opts)
+  model.setgpu(0)
+  model.resume(os.path.join(opts.checkpoint_dir, "best.pth"), train=False)
+  model.eval()
+
+  test_st_psnr = numpy.zeros((test_data_s.shape[0],), numpy.float32)
+  test_ts_psnr = numpy.zeros((test_data_t.shape[0],), numpy.float32)
+  test_st_ssim = numpy.zeros((test_data_s.shape[0],), numpy.float32)
+  test_ts_ssim = numpy.zeros((test_data_t.shape[0],), numpy.float32)
+  test_st_mae = numpy.zeros((test_data_s.shape[0],), numpy.float32)
+  test_ts_mae = numpy.zeros((test_data_t.shape[0],), numpy.float32)
+  test_st_list = []
+  test_ts_list = []
+  msg_detail = ""
+  with torch.no_grad():
+    for i in range(len(test_data_s)):
+      test_st = numpy.zeros(test_data_s[i].shape, numpy.float32)
+      test_ts = numpy.zeros(test_data_t[i].shape, numpy.float32)
+      used = numpy.zeros(test_data_s[i].shape, numpy.float32)
+      for j in range(test_data_s[i].shape[0] - opts.input_dim_a + 1):
+        test_patch_s = torch.tensor(numpy.expand_dims(test_data_s[i][j:j + opts.input_dim_a, :, :], 0), device=device)
+        test_patch_t = torch.tensor(numpy.expand_dims(test_data_t[i][j:j + opts.input_dim_a, :, :], 0), device=device)
+
+        ret_st = model.test_forward_transfer(test_patch_s, test_patch_t, a2b=True)
+        ret_ts = model.test_forward_transfer(test_patch_t, test_patch_s, a2b=False)
+
+        test_st[j:j + opts.input_dim_a, :, :] += ret_st.cpu().detach().numpy()[0]
+        test_ts[j:j + opts.input_dim_a, :, :] += ret_ts.cpu().detach().numpy()[0]
+        used[j:j + opts.input_dim_a, :, :] += 1
+
+      assert used.min() > 0
+      test_st /= used
+      test_ts /= used
+
+      if opts.result_dir:
+        common_ixi.save_nii(test_ts, os.path.join(opts.result_dir, "syn_%d.nii.gz" % i))
+
+      st_psnr = common_metrics.psnr(test_st, test_data_t[i])
+      ts_psnr = common_metrics.psnr(test_ts, test_data_s[i])
+      st_ssim = ssim(test_st, test_data_t[i], data_range=2.)
+      ts_ssim = ssim(test_ts, test_data_s[i], data_range=2.)
+      st_mae = abs(test_st - test_data_t[i]).mean()
+      ts_mae = abs(test_ts - test_data_s[i]).mean()
+
+      test_st_psnr[i] = st_psnr
+      test_ts_psnr[i] = ts_psnr
+      test_st_ssim[i] = st_ssim
+      test_ts_ssim[i] = ts_ssim
+      test_st_mae[i] = st_mae
+      test_ts_mae[i] = ts_mae
+      test_st_list.append(test_st)
+      test_ts_list.append(test_ts)
+
+      msg_detail += "  %d_psnr: %f  ssim: %f  mae: %f\n" % (i, ts_psnr, ts_ssim, ts_mae)
+
+  msg = "  test_st_psnr:%f/%f  test_st_ssim:%f/%f  test_st_mae:%f/%f  test_ts_psnr:%f/%f  test_ts_ssim:%f/%f  test_ts_mae:%f/%f\n" % \
+        (test_st_psnr.mean(), test_st_psnr.std(), test_st_ssim.mean(), test_st_ssim.std(),
+         test_st_mae.mean(), test_st_mae.std(), test_ts_psnr.mean(), test_ts_psnr.std(),
+         test_ts_ssim.mean(), test_ts_ssim.std(), test_ts_mae.mean(), test_ts_mae.std())
+  print(msg)
+  print(msg_detail)
+
+  if opts.result_dir:
+    with open(os.path.join(opts.result_dir, "result.txt"), "w") as f:
+      f.write(msg + msg_detail)
+
+    numpy.save(os.path.join(opts.result_dir, "st_psnr.npy"), test_st_psnr)
+    numpy.save(os.path.join(opts.result_dir, "ts_psnr.npy"), test_ts_psnr)
+    numpy.save(os.path.join(opts.result_dir, "st_ssim.npy"), test_st_ssim)
+    numpy.save(os.path.join(opts.result_dir, "ts_ssim.npy"), test_ts_ssim)
+    numpy.save(os.path.join(opts.result_dir, "st_ssim.npy"), test_st_mae)
+    numpy.save(os.path.join(opts.result_dir, "ts_ssim.npy"), test_ts_mae)
+
+
+if __name__ == '__main__':
+  main()
---
+++ src/train_ixi.py
@@ -0,0 +1,137 @@
+import torch
+from options_ixi import TrainOptions
+from model import DRIT
+from saver import Saver
+import sys
+import os
+import numpy
+import pdb
+import skimage.io
+import platform
+
+
+if platform.system() == 'Windows':
+  sys.path.append(r"E:\我的坚果云\sourcecode\python\util")
+else:
+  sys.path.append("/home/chenxu/我的坚果云/sourcecode/python/util")
+import common_metrics
+import common_ixi
+
+def main():
+  # parse options
+  parser = TrainOptions()
+  opts = parser.parse()
+
+  if opts.gpu >= 0:
+      os.environ["CUDA_VISIBLE_DEVICES"] = str(opts.gpu)
+      device = torch.device("cuda")
+  else:
+      os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+      device = torch.device("cpu")
+
+  if not os.path.exists(opts.checkpoint_dir):
+    os.makedirs(opts.checkpoint_dir)
+
+  # daita loader
+  print('\n--- load dataset ---')
+  #torch.autograd.set_detect_anomaly(True)
+
+  dataset_s = common_ixi.Dataset(opts.dataroot, modality="t2", n_slices=opts.input_dim_a, debug=opts.debug)
+  dataset_t = common_ixi.Dataset(opts.dataroot, modality="t1", n_slices=opts.input_dim_a, debug=opts.debug)
+  dataloader_s = torch.utils.data.DataLoader(dataset_s, batch_size=opts.batch_size, shuffle=True, pin_memory=True, drop_last=True)
+  dataloader_t = torch.utils.data.DataLoader(dataset_t, batch_size=opts.batch_size, shuffle=True, pin_memory=True, drop_last=True)
+  if opts.do_validation:
+    val_data_t, val_data_s = common_ixi.load_test_data(opts.dataroot, "val")
+
+  # model
+  print('\n--- load model ---')
+  model = DRIT(opts)
+  model.setgpu(0)
+  if opts.resume is None:
+    model.initialize()
+    ep0 = -1
+    total_it = 0
+  else:
+    ep0, total_it = model.resume(opts.resume)
+  model.set_scheduler(opts, last_ep=ep0)
+  ep0 += 1
+  print('start the training at epoch %d'%(ep0))
+
+  # saver for display and output
+  saver = Saver(opts)
+
+  # train
+  print('\n--- train ---')
+  best_psnr = 0
+  for it in range(opts.max_epochs):
+    for batch_id, (data_s, data_t) in enumerate(zip(dataloader_s, dataloader_t)):
+      images_a = data_s["image"].to(device)
+      images_b = data_t["image"].to(device)
+
+      # update model
+      if (batch_id + 1) % opts.d_iter != 0 and batch_id < len(dataloader_s) - 2:
+        model.update_D_content(images_a, images_b)
+        continue
+      else:
+        model.update_D(images_a, images_b)
+        model.update_EG()
+
+    if (it + 1) % opts.display_freq == 0 and opts.do_validation:
+      val_st_psnr = numpy.zeros((val_data_s.shape[0], 1), numpy.float32)
+      val_ts_psnr = numpy.zeros((val_data_t.shape[0], 1), numpy.float32)
+      val_st_list = []
+      val_ts_list = []
+      with torch.no_grad():
+        for i in range(val_data_s.shape[0]):
+          val_st = numpy.zeros(val_data_s.shape[1:], numpy.float32)
+          val_ts = numpy.zeros(val_data_t.shape[1:], numpy.float32)
+          used = numpy.zeros(val_data_s.shape[1:], numpy.float32)
+          for j in range(val_data_s.shape[1] - opts.input_dim_a + 1):
+            val_patch_s = torch.tensor(val_data_s[i:i + 1, j:j + opts.input_dim_a, :, :], device=device)
+            val_patch_t = torch.tensor(val_data_t[i:i + 1, j:j + opts.input_dim_a, :, :], device=device)
+
+            ret_st = model.test_forward_transfer(val_patch_s, val_patch_t, a2b=True)
+            ret_ts = model.test_forward_transfer(val_patch_t, val_patch_s, a2b=False)
+
+            val_st[j:j + opts.input_dim_a, :, :] += ret_st.cpu().detach().numpy()[0]
+            val_ts[j:j + opts.input_dim_a, :, :] += ret_ts.cpu().detach().numpy()[0]
+            used[j:j + opts.input_dim_a, :, :] += 1
+
+          assert used.min() > 0
+          val_st /= used
+          val_ts /= used
+
+          st_psnr = common_metrics.psnr(val_st, val_data_t[i])
+          ts_psnr = common_metrics.psnr(val_ts, val_data_s[i])
+
+          val_st_psnr[i] = st_psnr
+          val_ts_psnr[i] = ts_psnr
+          val_st_list.append(val_st)
+          val_ts_list.append(val_ts)
+
+      msg = "Iter:%d  val_st_psnr:%f/%f  val_ts_psnr:%f/%f" % \
+             (it + 1, val_st_psnr.mean(), val_st_psnr.std(), val_ts_psnr.mean(), val_ts_psnr.std())
+      gen_images_test = numpy.concatenate([val_data_s[0], val_st_list[0], val_ts_list[0], val_data_t[0]], 2)
+      gen_images_test = numpy.expand_dims(gen_images_test, 0).astype(numpy.float32)
+      gen_images_test = common_ixi.generate_display_image(gen_images_test, is_seg=False)
+
+      if opts.display_dir:
+        try:
+          skimage.io.imsave(os.path.join(opts.display_dir, "gen_images_test.jpg"), gen_images_test)
+        except:
+          pass
+
+      if val_ts_psnr.mean() > best_psnr:
+        best_psnr = val_ts_psnr.mean()
+
+        if best_psnr > opts.psnr_threshold:
+          model.save(os.path.join(opts.checkpoint_dir, "best.pth"), it, opts.max_epochs)
+
+      msg += "  best_ts_psnr:%f" % best_psnr
+      print(msg)
+
+  model.save(os.path.join(opts.checkpoint_dir, "final.pth"), it, opts.max_epochs)
+
+
+if __name__ == '__main__':
+  main()
